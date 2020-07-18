e5ffb2b70bf1ebcef0898429a79892c314439cc3
muhammadiskandar171 <muhammadiskandar934@gmail.com>
2020-07-18 15:02:42 +0700

    pert 11 : membuat Halaman template

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,10 @@
+<h4>main menu</h4>
+<div class="list-grup"list-group-flush>
+  <a href="#" class="list-group-item list-group-item-action">mahasiswa</a>
+  <a href="#" class="list-group-item list-group-item-action">dosen</a>
+
+
+  
+  
+
+</div>
---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', 'Halaman Mahasiswa')
+@section('bread1', ' Mahasiswa')
+@section('bread2', 'daftar')
+
+
+@section('content')
+   <p>This is my body content
+
+    <p>This is appended to the master sidebar.</p>
+@endsection
+
+@section('content')
+    <p>This is my body content.</p>
+@endsection
